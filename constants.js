139ca8f7d0e1507f78dc5de69f6eec4c3c02ec67
Ameer Hamza <55062550+ameeratgithub@users.noreply.github.com>
2024-01-22 12:51:41 +0500

    Constants updated

--- constants.js
+++ constants.js
@@ -1,5 +1,8 @@
 module.exports = {
+  // To download 1000 images per day, wait time should be 90 seconds (90000) milliseconds
+  // IMAGE_DOWNLOAD_WAIT_TIME: 90000,
   IMAGE_DOWNLOAD_WAIT_TIME: 5000,
+
   PAGE_LOAD_WAIT_TIME: 10000,
   SCROLL_WAIT_TIME: 10000,
 };
